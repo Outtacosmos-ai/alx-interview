c6af2b335a6fa8a4844cd197958b6b87ab76a96c
Outtacosmos-ai <messghir4@gmail.com>
2024-11-15 21:17:38 +0100

    Go with the flow

---
+++ 0x06-starwars_api/eslint.config.mjs
@@ -0,0 +1,11 @@
+export default [
+  {
+    ignores: ['node_modules'],
+  },
+  {
+    plugins: {}, // Remove 'react' plugin here
+    rules: {
+      // Define any custom rules here
+    },
+  },
+];
